18a73d68a4ea3c35871ef7c16a34049380b287a8
Kishore <kishore.sai37@gmail.com>
2021-07-15 18:20:47 +0530

    functionality completed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,34 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+//Getting unique category names
+const categoryNames = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(categoryNames);
+  const filterMenuItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    let newMenuItems = items.filter(
+      (menuItem) => menuItem.category === category
+    );
+    setMenuItems(newMenuItems);
+  };
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories categories={categories} filterMenuItems={filterMenuItems} />
+        <Menu menuItems={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterMenuItems, categories }) => {
+  return (
+    <div className='btn-container'>
+      {categories.map((category, index) => {
+        return (
+          <button
+            type='button'
+            className='filter-btn'
+            key={index}
+            onClick={() => filterMenuItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
